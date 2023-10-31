d43a56565f18da49021d2ef24ff7b1d6709fe05a
WANDJI <s-kwandji@lwsd.org>
2023-10-31 12:42:43 -0700

    update

---
+++ player-state.py
@@ -0,0 +1,10 @@
+
+
+class playerState():
+    def __init__(self,hp: int, isBlocking: bool, inAninmation: False):
+        self.hp = hp
+        self.isBlocking = isBlocking
+        self.inAnimation = inAninmation
+        
+    
+    def 
