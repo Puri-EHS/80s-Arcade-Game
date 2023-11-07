89d6926f1e932730c7a5428b4a1b93f2ab042272
kwandji113 <112978217+kwandji113@users.noreply.github.com>
2023-11-07 13:39:54 -0800

    Delete playerState.py

--- playerState.py
+++
@@ -1,7 +0,0 @@
-
-
-class playerState():
-    def __init__(self, hp: int, isBlocking: bool, inAninmation: False):
-        self.hp = hp
-        self.isBlocking = isBlocking
-        self.inAnimation = inAninmation
