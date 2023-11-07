e9a9102338b842a0db6358535e1bbdc05e2c53ae
satishvaibhav <s-vsatish@lwsd.org>
2023-11-07 13:16:43 -0800

    made changes to the spritesheet class

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -1,4 +1,25 @@
 import pygame
+#https://ehmatthes.github.io/pcc_2e/beyond_pcc/pygame_sprite_sheets/#a-simple-sprite-sheet
 class spritesheet:
-    def __init__(self, filename, row, col):
+    def __init__(self, filename):
         self.sheet = pygame.image.load(filename)
+    def get_image(self, rectangle, color):
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0,0), rect)
+        if color is not None:
+            if color is not -1:
+                color = image.get_at((0,0))
+                image.set_colorkey(color, pygame.RLEACCEL)
+        return image
+    def images_at(self, rects, colorkey = None):
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey = None):
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey) 
+class test: 
+    sprite = spritesheet("Blanka.png")
+
+        
