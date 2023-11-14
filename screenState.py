790b7123437769691979025a3657d80d55696d67
laubenjamin <s-blau@lwsd.org>
2023-11-14 13:34:08 -0800

    added player select text

--- screenState.py
+++ screenState.py
@@ -21,10 +21,11 @@ class screenState():
         self.rect = pygame.Rect(100, 150, 120, 100)
         self.screens = []
         self.button_pos = [0, 0]
-        self.char_selected = 0
+        self.char_selected = []
+        self.num_char_selected = 0
         self.char_buttons = ["Balrog", "Blanka", "Chun Li", "Chalsim", "E Honda", "Guile", "Ken", "M Bison", "Ryu", "Sagat", "Vega", "Vega"]
         self.map_buttons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-        self.map_selected = None
+        self.map_selected = BLUE
 
     def update_screen(self, events, players):
         if self.current_screen == 0:
@@ -48,9 +49,10 @@ class screenState():
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if self.select_controls(event.key):
-                    players[f"{self.char_selected}"] = self.char_buttons[self.button_pos[0] + self.button_pos[1]*4]
-                    self.char_selected += 1
-                    if self.char_selected == 2:
+                    self.char_selected.append(self.button_pos)
+                    players[f"{self.num_char_selected}"] = self.char_buttons[self.button_pos[0] + self.button_pos[1]*4]
+                    self.num_char_selected += 1
+                    if self.num_char_selected == 2:
                         self.current_screen += 1
         self.draw_select_boxes(True)
         return players
@@ -64,11 +66,11 @@ class screenState():
         self.draw_select_boxes(False)
 
     def fight_screen(self):
-        self.game_screen.fill(BLUE)
+        self.game_screen.blit(self.map_selected)
 
     def draw_select_boxes(self, char: bool):
         self.game_screen.blit(self.select_screen_background, self.select_screen_background.get_rect())
-        x = 100
+        x = 125
         y = 150
         for i in range(3):
             for j in range(4):
@@ -79,15 +81,29 @@ class screenState():
                 pygame.draw.rect(self.game_screen, (0, 255, 0), self.rect, 3)
                 if char:
                     text_surface = self.font.render(self.char_buttons[(i*4) + j], True, (0, 255, 0))
-                    self.game_screen.blit(pygame.image.load(os.path.join('char_select_img', 'IMG_878' + str(i + 1) + '.gif')), self.rect)
+                    self.game_screen.blit(pygame.image.load(os.path.join('char_select_img', f'{self.char_buttons[i*4 + j]}' + '.gif')), self.rect)
                 else:
                     text_surface = self.font.render(self.map_buttons[(i*4) + j], True, (0, 255, 0))
-                    self.game_screen.blit(pygame.image.load(os.path.join('char_select_img', 'IMG_878' + str(i + 1) + '.gif')), self.rect)
+                    self.game_screen.blit(pygame.image.load(os.path.join('char_select_img', f'{self.map_buttons[i*4 + j]}' + '.gif')), self.rect)
                 text_rect = text_surface.get_rect(center=self.rect.center)
                 self.game_screen.blit(text_surface, text_rect)
                 x += 150
-            x = 100
+            x = 125
             y += 150
+        
+    def draw_selected_char(self):
+        player1_text = self.font.render("Player 1:", 1, (0, 0, 0))
+        text_rect = self.rect
+        text_rect.x = 125
+        text_rect.y = 10
+        self.game_screen.blit(player1_text, text_rect)
+        player2_text = self.font.render("Player 2:", 1, (0, 0, 0))
+        text_rect.x = 500
+        text_rect.y = 10
+        self.game_screen.blit(player2_text, text_rect)
+        for x in self.char_selected:
+            self.game_screen.blit(pygame.image.load(os.path.join('char_select_img', f'{x}''.gif')), text_rect)
+            text_rect.x -= 375
 
 
     def select_controls(self, key):
