bb112065ff384dbcd094848149cf15aaeb30806d
Vaibhav Satish <90006361+SatishVaibhav@users.noreply.github.com>
2023-11-14 12:23:32 -0800

    Update SpriteSheet.py

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -13,11 +13,11 @@ class spritesheet:
                 image.set_colorkey(color, pygame.RLEACCEL)
         return image
     def get_multiple_images(self, rects, colorkey = None):
-        return [self.image_at(rect, colorkey) for rect in rects]
+        return [self.get_image(rect, colorkey) for rect in rects]
 
     def load_strip(self, rect, image_count, colorkey = None):
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
-        return self.images_at(tups, colorkey) 
+        return self.get_multiple_images(tups, colorkey) 
 
         
