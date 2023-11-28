e793497dc5576f846f1ddb5e10818d92879f4ca2
NAMBIAR <s-nnambiar@lwsd.org>
2023-11-28 13:16:35 -0800

    upload imges

--- Game.py
+++ Game.py
@@ -14,7 +14,7 @@ font = pygame.font.Font(None, 36)
 
 background_image1 = pygame.image.load("testimage.jpg")
 background_image1 = pygame.transform.scale(background_image1, (screen_width, screen_height))
-background_image2 = pygame.image.load("characterscreen.jpg")
+background_image2 = pygame.image.load("Balrog.jpg")
 background_image2 = pygame.transform.scale(background_image2, (screen_width, screen_height))
 
 def draw_button(x, y, width, height, color, text, text_color):
@@ -33,17 +33,17 @@ def main():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
-                button1_rect = draw_button(300, 500, 200, 50, black, "Start", white)
+                button1_rect = draw_button(300, 500, 200, 50, black, "Back", white)
                 button2_rect = draw_button(575, 500, 200, 50, black, "Button 2", white)
                 if button1_rect.collidepoint(mouse_pos):
-                    current_background = background_image2
+                    return current_background
                 elif button2_rect.collidepoint(mouse_pos):
                     current_background = background_image2
 
 
         screen.blit(current_background, (0, 0))
-        draw_button(300, 500, 200, 50, black, "Start", white)
-        draw_button(575, 500, 200, 50, black, "Button 2", white)
+        draw_button(25, 500, 200, 50, black, "Back", white)
+        draw_button(575, 500, 200, 50, black, "Continue", white)
         pygame.display.update()
     pygame.quit()
 
