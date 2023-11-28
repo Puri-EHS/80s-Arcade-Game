f9ec1ddb3f8c119f98bdc3df375cc8963c7d1ff4
satishvaibhav <s-vsatish@lwsd.org>
2023-11-28 13:19:57 -0800

    Started and nearly finished power up implementation #21

--- playerState.py
+++ playerState.py
@@ -1,13 +1,28 @@
-
-
+#the first parameter is the powerup. the second parameter is the damage it does 
+character_powerups = {
+            "Ryu": ["Fireball", 70], 
+            "Balrog": ["Superpunch", 80], 
+            "Blanka": ["Energized Attack", 65], 
+            "Dhalsim": ["Fury Fire", 50],  
+            "Sagat":  ["Rapid Hit", 45],  
+            "Guile": ["Flash Kick", 40], 
+            "Vega": ["Super Claw Attack", 130], 
+            "Chun Li": ["Power Kick", 25],  
+            "Zangief": ["Lariat", 15], 
+            "E Honda": ["Super Might", 100], 
+            "M Bison": ["Super Strength", 110], 
+            "Ken": ["Super Speed", 50]
+        }
 class playerState():
     def __init__(self, champion: str, hp : int, isBlocking: bool, inAninmation: False):
         self.champion = champion
         self.hp = hp
         self.isBlocking = isBlocking
         self.inAnimation = inAninmation
+        self.MIN_HP_NUM = 0
+        self.powerup_usable = False
         self.champAnimations = {}
-        
+        self.character_powerup_name = None
     
     def load_animations(self, champion):
         
@@ -20,13 +35,24 @@ class playerState():
         
         """
         
-        
 
-    def updateHp(self, attack):
+    def updateHp(self, attack, power_up:None):
         """Will update the amount of helath remaining based on
             the attack the user was hit with  
 
         Args:
             attack (_type_): _description_
         """
+    def setPowerup(self):
+        if self.powerup_usable:
+            self.character_powerup_name = character_powerups[self.champion][0]  
+    def usePowerup(self):
+        character_powerup_damage = character_powerups[self.champion][1]
+        self.updateHp(None, character_powerup_damage)
+
+            
+
+
+
+
         
