59a2e5667d392ec765cb1be68b2a6a11b81b7f57
laubenjamin <s-blau@lwsd.org>
2023-11-28 12:46:13 -0800

    working on load animation func

--- playerState.py
+++ playerState.py
@@ -1,4 +1,5 @@
-
+import pygame
+import os
 
 class playerState():
     def __init__(self, champion: str, hp : int, isBlocking: bool, inAninmation: False):
@@ -6,21 +7,21 @@ class playerState():
         self.hp = hp
         self.isBlocking = isBlocking
         self.inAnimation = inAninmation
+        self.cur_powerups = None
         self.champAnimations = {}
+        self.start_frame = 0
         
     
     def load_animations(self, champion):
         
         """Will load the animations for the current champion into 
             champAnimations, and populate the dictinonary"""
-            
+        self.images = pygame.image.load()
     
-    def update(self):
+    def update(self, events, frames):
         """ Will update after any action is taken
         
         """
-        
-        
 
     def updateHp(self, attack):
         """Will update the amount of helath remaining based on
