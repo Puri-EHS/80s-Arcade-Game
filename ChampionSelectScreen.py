2f0d164069008a1d6e13050f20359c78be0342b2
ChowdharyAarna <s-achowdhary@lwsd.org>
2024-01-29 23:08:35 -0800

    updated champion select to only include our chosen 4 characters

--- ChampionSelectScreen.py
+++ ChampionSelectScreen.py
@@ -7,10 +7,10 @@ class ChampionSelectScreen():
     def __init__(self, game_screen, screen_width, screen_height) -> None:
         self.screen = game_screen
         self.font = pygame.font.Font(None, 36)
-        self.char_buttons = ["Balrog", "Blanka", "Chun Li", "Dhalsim", "E Honda", "Guile", "Ken", "M Bison", "Ryu", "Sagat", "Vega", "Zangief"]
+        self.char_buttons = ["Balrog", "Dhalsim", "Ken", "Ryu"]
         self.background = pygame.image.load(os.path.join('Backgrounds', "Character Select Background.jpg"))
         self.background = pygame.transform.scale(self.background, (screen_width, screen_height))
-        self.rect = pygame.Rect(100, 150, 120, 100)
+        self.rect = pygame.Rect(100, 150, 220, 200)
         self.button_pos = [0,0]
         self.char_selected = []
 
@@ -19,9 +19,9 @@ class ChampionSelectScreen():
             if event.type == pygame.KEYDOWN:
                 if self.select_controls(event.key):
                     if len(self.char_selected) == 0:
-                        player = player1(self.char_buttons[self.button_pos[0] + self.button_pos[1]*4])
+                        player = player1(self.char_buttons[self.button_pos[0] + self.button_pos[1]*2])
                     else:
-                        player = player2(self.char_buttons[self.button_pos[0] + self.button_pos[1]*4])
+                        player = player2(self.char_buttons[self.button_pos[0] + self.button_pos[1]*2])
                     self.char_selected.append(player)
         self.draw_select_boxes(True)
         if len(self.char_selected) == 2:
@@ -30,26 +30,29 @@ class ChampionSelectScreen():
     
     def draw_select_boxes(self, char: bool):
         self.screen.blit(self.background, self.background.get_rect())
-        x = 125
+        x = 145
         y = 150
-        for i in range(3):
-            for j in range(4):
+        for i in range(2):
+            for j in range(2):
                 self.rect.x = x
                 self.rect.y = y
                 if i == self.button_pos[1] and j == self.button_pos[0]:
                     pygame.draw.rect(self.screen, (0, 0, 255), self.rect)
-                pygame.draw.rect(self.screen, (0, 255, 0), self.rect, 3)
-                text_surface = self.font.render(self.char_buttons[(i*4) + j], True, (0, 255, 0))
-                img = pygame.image.load(os.path.join('char_select_img', f'{self.char_buttons[i*4 + j]}' + '.gif')).convert_alpha()
+                pygame.draw.rect(self.screen, (15, 155, 186), self.rect, 3)
+                text_surface = self.font.render(self.char_buttons[(i*2) + j], True, (15, 155, 186))
+                img = pygame.image.load(os.path.join('char_select_img', f'{self.char_buttons[i*2 + j]}' + '.gif')).convert_alpha()
                 img.set_colorkey((255,255,255))
-                if img.get_rect().x > 120:
-                    img = pygame.transform.scale(img, (120,100))
-                self.screen.blit(img, self.rect)
+                # if img.get_rect().x > 220:
+                img = pygame.transform.scale(img, (210,190))
+                img_rect = self.rect
+                img_rect.x += 5
+                img_rect.y += 5
+                self.screen.blit(img, img_rect)
                 text_rect = text_surface.get_rect(center=self.rect.center)
                 self.screen.blit(text_surface, text_rect)
-                x += 150
-            x = 125
-            y += 150
+                x += 300
+            x = 145
+            y += 230
 
     
     def select_controls(self, key):
@@ -68,8 +71,8 @@ class ChampionSelectScreen():
         if self.button_pos[0] < 0:
             self.button_pos[0] = 3
         else:
-            self.button_pos[0] = abs(self.button_pos[0])%4
-        self.button_pos[1] = self.button_pos[1]%3
+            self.button_pos[0] = abs(self.button_pos[0])%2
+        self.button_pos[1] = self.button_pos[1]%2
         return False
     
     def reset(self):
